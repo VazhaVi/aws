6632e339ade2bc9bf573670c665c1f53296e80a3
Vazha <109624359+VazhaVi@users.noreply.github.com>
2022-11-29 16:03:29 +0400

    inital commit

---
+++ 0-aws-provider.tf
@@ -0,0 +1,19 @@
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
