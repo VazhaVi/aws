cf2ff057d9eea3b6dd676ddce1b415a0f8711718
bekakochauri <bekakochauri@admins-MacBook-Air.local>
2022-12-04 00:20:27 +0400

    enable private & public kube-api

--- 2-eks.tf
+++ 2-eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_version = "1.23"
 
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = false
+  cluster_endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
